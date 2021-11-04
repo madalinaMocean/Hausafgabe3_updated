91034054532eb041a6eabf23b223ffdc6869fd97
madalinaMocean <maria.madalina.mocean@stud.ubbcluj.ro>
2021-11-04 14:14:54 +0200

    Java conventions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,33 +10,33 @@ public class Main {
     public static void main(String[] args) {
         RegistrationSystem registrationSystem = new RegistrationSystem();
 
-        Teacher t1 = new Teacher("Dan", "Doe");
-        Teacher t2 = new Teacher("Mary", "Jane");
+        Teacher teacher1 = new Teacher("Dan", "Doe");
+        Teacher teacher2 = new Teacher("Mary", "Jane");
 
-        Course c1 = new Course("APM", t1, 1, 6);
-        registrationSystem.courseList.create(c1); // add course to registration
-        Course c2 = new Course("DSA", t2, 3, 6);
-        registrationSystem.courseList.create(c2);
+        Course course1 = new Course("APM", teacher1, 1, 6);
+        registrationSystem.courseList.create(course1); // add course to registration
+        Course course2 = new Course("DSA", teacher2, 3, 6);
+        registrationSystem.courseList.create(course2);
 
-        Student s1 = new Student("James", "Blonde", 1);
-        Student s2 = new Student("Jane", "Flower", 1);
+        Student student1 = new Student("James", "Blonde", 1);
+        Student student2 = new Student("Jane", "Flower", 1);
 
-        registrationSystem.register(c1,s1); // can register
-        registrationSystem.register(c2,s1); // can register
-        registrationSystem.register(c1,s2); // cannot register - course 1 is full
-        registrationSystem.register(c2,s2); // re-tries to register to another course
+        registrationSystem.register(course1,student1); // can register
+        registrationSystem.register(course2,student1); // can register
+        registrationSystem.register(course1,student2); // cannot register - course 1 is full
+        registrationSystem.register(course2,student2); // re-tries to register to another course
 
         System.out.print("Available Courses: ");
         registrationSystem.retrieveCoursesWithFreePlaces().forEach( course -> System.out.print(course.getCourseName()+", "));
         System.out.println();
 
         System.out.print("Students enrolled in APM Course: ");
-        registrationSystem.retrieveStudentsEnrolledForACourse(c1)
+        registrationSystem.retrieveStudentsEnrolledForACourse(course1)
                 .forEach( student -> System.out.print(student.getFirstName()+" "+student.getLastName()+", "));
         System.out.println();
 
         System.out.print("Students enrolled in DSA Course: ");
-        registrationSystem.retrieveStudentsEnrolledForACourse(c2)
+        registrationSystem.retrieveStudentsEnrolledForACourse(course2)
                 .forEach( student -> System.out.print(student.getFirstName()+" "+student.getLastName()+", "));
     }
 }
--- test/java/com/company/controllers/RegistrationSystemTest.java
+++ test/java/com/company/controllers/RegistrationSystemTest.java
@@ -13,55 +13,55 @@ import static org.junit.jupiter.api.Assertions.*;
 class RegistrationSystemTest {
     RegistrationSystem registrationSystem = new RegistrationSystem();
 
-    Teacher t1 = new Teacher("Dan", "Doe");
-    Teacher t2 = new Teacher("Mary", "Jane");
+    Teacher teacher1 = new Teacher("Dan", "Doe");
+    Teacher teacher2 = new Teacher("Mary", "Jane");
 
-    Course c1 = new Course("APM", t1, 1, 6);
-    Course c2 = new Course("DSA", t2, 3, 6);
+    Course course1 = new Course("APM", teacher1, 1, 6);
+    Course course2 = new Course("DSA", teacher2, 3, 6);
 
-    Student s1 = new Student("James", "Blonde", 1);
-    Student s2 = new Student("Jane", "Flower", 1);
+    Student student1 = new Student("James", "Blonde", 1);
+    Student student2 = new Student("Jane", "Flower", 1);
 
     @Test
     void register() {
-        assert(registrationSystem.register(c1,s1));
-        assert(!registrationSystem.register(c1,s2));
-        assert(!registrationSystem.register(c1,s1));
+        assert(registrationSystem.register(course1,student1));
+        assert(!registrationSystem.register(course1,student2));
+        assert(!registrationSystem.register(course1,student1));
     }
 
     @Test
     void retrieveCoursesWithFreePlaces() {
         assert(registrationSystem.retrieveCoursesWithFreePlaces().isEmpty());
-        registrationSystem.courseList.create(c1);
-        registrationSystem.register(c1,s1);
+        registrationSystem.courseList.create(course1);
+        registrationSystem.register(course1,student1);
         assert(registrationSystem.retrieveCoursesWithFreePlaces().isEmpty());
-        registrationSystem.courseList.create(c2);
+        registrationSystem.courseList.create(course2);
         assert(registrationSystem.retrieveCoursesWithFreePlaces().size()==1);
     }
 
     @Test
     void retrieveStudentsEnrolledForACourse() {
-        registrationSystem.courseList.create(c1);
-        registrationSystem.register(c1,s1);
-        assert(registrationSystem.retrieveStudentsEnrolledForACourse(c1).size()==1);
+        registrationSystem.courseList.create(course1);
+        registrationSystem.register(course1,student1);
+        assert(registrationSystem.retrieveStudentsEnrolledForACourse(course1).size()==1);
     }
 
     @Test
     void getAllCourses() {
-        registrationSystem.courseList.create(c1);
-        registrationSystem.courseList.create(c2);
+        registrationSystem.courseList.create(course1);
+        registrationSystem.courseList.create(course2);
         assert(registrationSystem.getAllCourses().size()==2);
     }
 
     @Test
     void deleteCourseByTeacher() {
-        registrationSystem.courseList.create(c1); // add course to registration
-        registrationSystem.register(c1,s1);
+        registrationSystem.courseList.create(course1); // add course to registration
+        registrationSystem.register(course1,student1);
         List<Course> t1c = new ArrayList<Course>();
-        t1c.add(c1);
-        t1.setCourses(t1c);
-        assert(!t1.getCourses().isEmpty());
-        registrationSystem.deleteCourseByTeacher(t1, c1);
-        assert(t1.getCourses().isEmpty());
+        t1c.add(course1);
+        teacher1.setCourses(t1c);
+        assert(!teacher1.getCourses().isEmpty());
+        registrationSystem.deleteCourseByTeacher(teacher1, course1);
+        assert(teacher1.getCourses().isEmpty());
     }
 }
